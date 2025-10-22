075e0e873a0335707383d92ad115e6cdd12bb499
anarghya-das <anarghya@buffalo.edu>
2025-10-22 02:41:23 -0400

    added pre-training and refactored conifg directory

--- cerebro/models/labram/__init__.py
+++ cerebro/models/labram/__init__.py
@@ -4,14 +4,17 @@ This module contains implementations for:
 - VQNSP: Vector Quantized Neural Signal Processing for EEG tokenization
 - NeuralTransformer: Transformer architecture for EEG analysis
 - NormEMAVectorQuantizer: Normalized EMA-based vector quantization
+- MEMPretrainModule: Masked EEG Modeling pretraining module
 """
 
 from .tokenizer import VQNSP
 from .modeling_finetune import NeuralTransformer
 from .norm_ema_quantizer import NormEMAVectorQuantizer
+from .pretrain import MEMPretrainModule
 
 __all__ = [
     "VQNSP",
     "NeuralTransformer",
     "NormEMAVectorQuantizer",
+    "MEMPretrainModule",
 ]
